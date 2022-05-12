2c75f44ef66dcc197dbad8ba6aa78aaa37e778ef
Abhishek Rajesh Agarwal <62901950+Abishek1710@users.noreply.github.com>
2022-05-12 17:41:05 +0530

    Update remaster.py
    Update file

--- Remastering/remaster.py
+++ Remastering/remaster.py
@@ -1,17 +1,3 @@
-"""
-   Copyright (C) <2019> <Satoshi Iizuka and Edgar Simo-Serra>
-
-   This work is licensed under the Creative Commons
-   Attribution-NonCommercial-ShareAlike 4.0 International License. To view a copy
-   of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or
-   send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
-
-   Satoshi Iizuka, University of Tsukuba
-   iizuka@aoni.waseda.jp, http://iizuka.cs.tsukuba.ac.jp/index_eng.html
-   Edgar Simo-Serra, Waseda University
-   ess@waseda.jp, https://esslab.jp/~ess/
-"""
-
 import torch
 import torch.nn as nn
 from torchvision import transforms
